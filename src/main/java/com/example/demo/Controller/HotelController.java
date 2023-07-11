27c4922475bfe80af334677beda297f721054567
mohammadsabih6 <sabih.tariq@xloopdigital.com>
2023-07-12 02:55:26 +0500

    GetByID updated

--- src/main/java/com/example/demo/Controller/HotelController.java
+++ src/main/java/com/example/demo/Controller/HotelController.java
@@ -1,10 +1,13 @@
 package com.example.demo.Controller;
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,6 +38,16 @@ public class HotelController {
         else{
             String errorMessage = "Unable to get Counselor data";
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorMessage);
+        }
+    }
+    @GetMapping("/get/{id}")
+    public ResponseEntity<?> getNyID(@PathVariable Long id) {
+        Optional<Hotel> hotel = hotelRepo.findById(id);
+
+        if (hotel != null) {
+            return ResponseEntity.ok(hotel);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Hotel not found");
         }
     }
     @PostMapping("/T_post")
